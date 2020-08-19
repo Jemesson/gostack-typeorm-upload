7c638e1270e07f6db426f4081ad258c692e098e6
Jemesson <jemessonlima@gmail.com>
2020-08-19 14:15:51 -0300

    Add database upload challenge phase2

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,32 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const balanceFn = (accumulator: Balance, current: Transaction): Balance => {
+      switch (current.type) {
+        case 'income':
+          accumulator.income += Number(current.value);
+          break;
+        case 'outcome':
+          accumulator.outcome += Number(current.value);
+          break;
+        default:
+          break;
+      }
+
+      return accumulator;
+    };
+
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(balanceFn, {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    });
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
